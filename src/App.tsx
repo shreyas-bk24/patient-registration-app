b9f0a5993cd242a8b4057dbde6d69c5c2c68a3cd
shreyas-bk24 <shreyasbk41@gmail.com>
2025-05-20 10:27:31 +0530

    feat: initialize patient registration app with React, Vite, and TypeScript
    -Add scaffold MUI Joy layout with Home, PatientForm, PatientList, SqlQuery components

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import HomePage from './components/HomePage'
+
+
+function App() {
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Tabs from '@mui/joy/Tabs';
+import TabList from '@mui/joy/TabList';
+import Tab from '@mui/joy/Tab';
+import TabPanel from '@mui/joy/TabPanel';
+import { Box, Typography } from '@mui/joy';
+
+
+const HomePage = () => {
+    
+    return (
+        <Box sx={{ p: 3, bgcolor: 'background.paper' }}>
+            <Typography level='h3' gutterBottom>Patient Registration System</Typography>
+            <Tabs aria-label="Tabs with icons" variant="plain" defaultValue={0}>
+                <TabList>
+                    <Tab>Patient Registration</Tab> 
+                    <Tab>Patient List</Tab>
+                </TabList>
+                <TabPanel value={0}>
+                    <p>Patient Registration form</p>
+                </TabPanel>
+                <TabPanel value={1}>
+                    <p>Patient list and SQL Query tab</p>
+                </TabPanel>
+            </Tabs>
+        </Box>
+    );
+
+}
+
+export default HomePage;
